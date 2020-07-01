94d65dc3d10de4c3c061a7683e2c8cf3d7324e71
Kevin Petschik <kevin.petschik@hs-furtwangen.de>
2020-07-01 19:05:46 +0200

    Files aktuallisiert

--- praktikum/src/test/java/EasyMockTest.java
+++ praktikum/src/test/java/EasyMockTest.java
@@ -1,62 +1,98 @@
-import static org.junit.Assert.*;
-
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.easymock.EasyMock.*;
-
-import org.junit.Test;
-
 import de.hfu.residents.domain.Resident;
 import de.hfu.residents.repository.ResidentRepository;
-import de.hfu.residents.repository.ResidentRepositoryStub;
 import de.hfu.residents.service.BaseResidentService;
+import de.hfu.residents.service.ResidentServiceException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 
 public class EasyMockTest {
 
-	@Test
-	public void test() {
-		Konto quelle = new Konto("1", 1000.0);
-		Konto ziel = new Konto("2", 100.0);
-		KontoManager kontoManagerMock = createMock(KontoManager.class);
-		expect(kontoManagerMock.findeKonto("1")).andReturn(quelle);
-		expect(kontoManagerMock.findeKonto("2")).andReturn(ziel);
-		kontoManagerMock.updateKonto(quelle);
-		kontoManagerMock.updateKonto(ziel);
-		replay(kontoManagerMock);
-		KontoService service = new KontoService();
-		service.setKontoManager(kontoManagerMock);
-		service.transfer("1", "2", 500.0);
-		verify(kontoManagerMock);
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// ResidentRepository residentRepository = new ResidentRepositoryStub();
-		BaseResidentService residentService = new BaseResidentService();
-		
-		Resident r1 = new Resident("Vorname", "Nachname", "Strasse", "Stadt", null);
-		Resident r2 = new Resident("Vorname2", "Nachname2", "Strasse2", "Stadt2", null);
-		Resident r3 = new Resident("Max", "Mustermann", "Musterstrasse", "Musterstadt", null);
-		
-		ResidentRepository residentRepositoryMock = createMock(ResidentRepository.class);
-		expect(residentRepositoryMock);
-		
-		((ResidentRepositoryStub) residentRepository).addResident(r1);
-		((ResidentRepositoryStub) residentRepository).addResident(r2);
-		((ResidentRepositoryStub) residentRepository).addResident(r3);
-		
-		residentService.setResidentRepository(residentRepository);
-		
-		Resident filter = new Resident("M*", "*", "", "*", null);
-		List<Resident> filterResult = residentService.getFilteredResidentsList(filter);
-		
-		for(int i = 0; i < filterResult.size(); i++) {
-			if(filterResult.get(i).equals(r3)) {
-				assertEquals(r3, filterResult.get(i));
-			} else {
-				assertEquals(null, filterResult.get(i));
-			}
-		}
+    private ResidentRepository mockResidentRepository;
+    private BaseResidentService baseResidentService = new BaseResidentService();
 
-	}
+    private Resident r1;
+    private Resident r2;
+    private Resident r3;
 
-}
+
+    public EasyMockTest() {
+    	
+        r1 = new Resident("Vorname", "Nachname", "Strasse", "Stadt", null);
+		r2 = new Resident("Vorname2", "Nachname2", "Strasse2", "Stadt2", null);
+		r3 = new Resident("Max", "Mustermann", "Musterstrasse", "Musterstadt", null);
+		
+        ArrayList<Resident> residents = new ArrayList<Resident>();
+        residents.add(r1);
+        residents.add(r2);
+        residents.add(r3);
+        
+        mockResidentRepository = createMock(ResidentRepository.class);
+        
+        expect(mockResidentRepository.getResidents()).andReturn(residents);
+        replay(mockResidentRepository);
+        
+        baseResidentService.setResidentRepository(mockResidentRepository);
+    }
+
+    @Test(expected = ResidentServiceException.class, timeout = 1000)
+    public void testGetUniqueResidentWithWildcard() throws Exception {
+        Resident r = new Resident("TestMitWildcard*", "", "", "", null);
+        baseResidentService.getUniqueResident(r);
+        verify(mockResidentRepository);
+    }
+
+
+    @Test(expected = ResidentServiceException.class, timeout = 1000)
+    public void testGetUniqueResidentNoResult() throws Exception {
+        Resident r = new Resident("Test", "", "", "", null);
+        baseResidentService.getUniqueResident(r);
+        verify(mockResidentRepository);
+    }
+
+
+    @Test
+    public void testGetUniqueResident() throws Exception {
+        Resident r = new Resident("Vorname", "Nachname", "Strasse", "Stadt", null);
+        Assert.assertThat(r.getFamilyName(), equalTo(baseResidentService.getUniqueResident(r).getFamilyName()));
+        verify(mockResidentRepository);
+    }
+
+
+    @Test
+    public void testGetFilteredResidentsListWithWildcard() {
+        Resident r = new Resident("", "Na*", "", "", null);
+        Assert.assertThat(Arrays.asList(r1, r2), equalTo(baseResidentService.getFilteredResidentsList(r)));
+        verify(mockResidentRepository);
+    }
+
+    
+    @Test
+    public void testGetFilteredResidentsList() throws Exception {
+        Resident r = new Resident("", "Nachname", "", "", null);
+        Assert.assertThat(Arrays.asList(r1), equalTo(baseResidentService.getFilteredResidentsList(r)));
+        verify(mockResidentRepository);
+    }
+
+    
+    @Test
+    public void testGetFilteredResidentsListNoResult() throws Exception {
+        Resident r = new Resident("error", "", "", "", null);
+        Assert.assertThat(Arrays.asList(), equalTo(baseResidentService.getFilteredResidentsList(r)));
+        verify(mockResidentRepository);
+    }
+} 
